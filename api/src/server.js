0173a2d224c388a84a1e09d406262403dfbe6368
julesdocx <jules.docx@student.ehb.be>
2021-08-05 10:49:23 +0200

    fix(integration.test)

--- api/src/server.js
+++ api/src/server.js
@@ -6,126 +6,118 @@ const Helpers = require('./utils/helpers.js')
 const port = 3000
 
 
-const pg = require('knex')({
-  client: 'pg',
-  version: '9.6',      
-  searchPath: ['knex', 'public'],
-  connection: process.env.PG_CONNECTION_STRING ? process.env.PG_CONNECTION_STRING : 'postgres://example:example@localhost:5432/test'
-});
-
-
 const app = express();
 http.Server(app); 
 
 
-app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    // to support URL-encoded bodies
-    extended: true
-  })
-);  
+// app.use(bodyParser.json());
+// app.use(
+//   bodyParser.urlencoded({
+//     // to support URL-encoded bodies
+//     extended: true
+//   })
+// );  
 
-app.get('/test', (req, res) => {
+// app.get('/test', (req, res) => {
   
-  res.status(200).send();
-})
+//   res.status(200).send();
+// })
 
-app.get('/', async (req, res) => {
-  const result = await pg
-    .select(['uuid', 'title', 'created_at'])
-    .from('story')
-  res.json({
-      res: result
-  })
-})
+// app.get('/', async (req, res) => {
+//   const result = await pg
+//     .select(['uuid', 'title', 'created_at'])
+//     .from('story')
+//   res.json({
+//       res: result
+//   })
+// })
 
-app.get('/story/:uuid', async (req, res) => {
-  const result = await pg
-    .select(['uuid', 'title', 'created_at'])
-    .from('story')
-    .where({uuid: req.params.uuid})
-  res.json({
-      res: result
-  })
-})
+// app.get('/story/:uuid', async (req, res) => {
+//   const result = await pg
+//     .select(['uuid', 'title', 'created_at'])
+//     .from('story')
+//     .where({uuid: req.params.uuid})
+//   res.json({
+//       res: result
+//   })
+// })
 
-/**     ///////////// GET ALL RECORD /////////////    */
+// /**     ///////////// GET ALL RECORD /////////////    */
 
-app.get('/storyblock', async (req, res) => {
-  const result = await pg.select(['uuid', 'content', 'story_id', 'created_at']).from('storyblock')
-  res.json({
-      res: result
-  })
-})
+// app.get('/storyblock', async (req, res) => {
+//   const result = await pg.select(['uuid', 'content', 'story_id', 'created_at']).from('storyblock')
+//   res.json({
+//       res: result
+//   })
+// })
 
-/**     ///////////// GET RECORD BY UUID /////////////    */
+// /**     ///////////// GET RECORD BY UUID /////////////    */
 
-app.get('/storyblock/:uuid', async (req, res) => {
+// app.get('/storyblock/:uuid', async (req, res) => {
 
-// GET
-// record
-// met UUID
+// // GET
+// // record
+// // met UUID
 
-})
+// })
 
-/**     ///////////// ADD RECORD /////////////    */
+// /**     ///////////// ADD RECORD /////////////    */
 
-app.post('/addstoryblock/', async (req, res) => {
+// app.post('/addstoryblock/', async (req, res) => {
 
-// ADD
-// record
+// // ADD
+// // record
 
-})
+// })
 
-/**     ///////////// DELETE RECORD /////////////    */
-app.delete('/storyblock/', async (req, res) => {
+// /**     ///////////// DELETE RECORD /////////////    */
+// app.delete('/storyblock/', async (req, res) => {
 
-// Delete
-// record
-// met UUID
+// // Delete
+// // record
+// // met UUID
 
-})
+// })
 
-async function initialiseTables() {
-  await pg.schema.hasTable('storyblock').then(async (exists) => {
-    if (!exists) {
-      await pg.schema
-        .createTable('storyblock', (table) => {
-          table.increments();
-          table.uuid('uuid');
-          table.string('content');
-          table.string('story_id');
-          table.integer('order');
-          table.timestamps(true, true);
-        })
-        .then(async () => {
-          console.log('created table storyblock');
-        });
+// async function initialiseTables() {
+//   await pg.schema.hasTable('storyblock').then(async (exists) => {
+//     if (!exists) {
+//       await pg.schema
+//         .createTable('storyblock', (table) => {
+//           table.increments();
+//           table.uuid('uuid');
+//           table.string('content');
+//           table.string('story_id');
+//           table.integer('order');
+//           table.timestamps(true, true);
+//         })
+//         .then(async () => {
+//           console.log('created table storyblock');
+//         });
 
-    }
-  });
-  await pg.schema.hasTable('story').then(async (exists) => {
-    if (!exists) {
-      await pg.schema
-        .createTable('story', (table) => {
-          table.increments();
-          table.uuid('uuid');
-          table.string('title');
-          table.string('summary');
-          table.timestamps(true, true);
-        })
-        .then(async () => {
-          console.log('created table story');
-          for (let i = 0; i < 10; i++) {
-            const uuid = Helpers.generateUUID();
-            await pg.table('story').insert({ uuid, title: `random element number ${i}` })
-          }
-        });
+//     }
+//   });
+//   await pg.schema.hasTable('story').then(async (exists) => {
+//     if (!exists) {
+//       await pg.schema
+//         .createTable('story', (table) => {
+//           table.increments();
+//           table.uuid('uuid');
+//           table.string('title');
+//           table.string('summary');
+//           table.timestamps(true, true);
+//         })
+//         .then(async () => {
+//           console.log('created table story');
+//           for (let i = 0; i < 10; i++) {
+//             const uuid = Helpers.generateUUID();
+//             await pg.table('story').insert({ uuid, title: `random element number ${i}` })
+//           }
+//         });
         
-    }
-  });
-}
-initialiseTables()
+//     }
+//   });
+// }
+//initialiseTables()
 
 module.exports = app;
